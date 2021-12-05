ed5b2af9d7238385eff00560c49f8f883f84b107
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2021-12-05 12:37:55 +0300

    feat: update news url

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {                //https://newsapi.org/v2/
             apiKey: '99c8a71ff8664332aab9d5f56ca1f58f',
         });
     }
